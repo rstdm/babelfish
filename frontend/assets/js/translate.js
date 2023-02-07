c5b7188fc753a95552c5bd98cc791a638869e775
Rene Maget <37067579+rstdm@users.noreply.github.com>
2023-02-07 12:47:20 +0100

    frontend retrieves translated text from backend

--- frontend/assets/js/translate.js
+++ frontend/assets/js/translate.js
@@ -60,11 +60,11 @@ function updateOutput() {
         const translation = getCachedSentence(sentenceEntry.sentence, "en", "de") // todo
         if (translation === undefined || translation === null) {
             if (insertLoadingSpan) {
-                span.innerHTML = "<i>Lädt...</i> "
+                span.innerHTML = " <i>Lädt...</i>"
                 insertLoadingSpan = false;
             }
         } else {
-            span.innerText = translation + " "
+            span.innerText = translation
             insertLoadingSpan = true;
         }
 
@@ -99,11 +99,35 @@ async function translateSentence(sentence, srcLang, dstLang) {
     }
     setCachedSentence(sentence, null, srcLang, dstLang)
 
-    await new Promise(r => setTimeout(r, 2000)); // TODO remove "sleep"
+    const requestBody = {
+        sourceLanguage: srcLang,
+        destinationLanguage: dstLang,
+        sourceText: sentence,
+    }
 
-    const translatedSentence = sentence;
+    const response = await fetch('http://localhost:8000/translate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody),
+    });
+
+    if (response.status !== 200){
+        const errorMessage = "Failed to retrieve translation from the server. URL: " + response.url +
+            " Status: " + response.status + " Response Body: " + await response.text();
+        throw new Error(errorMessage)
+    }
+    const responseBody = await response.json()
+    let translatedSentence = responseBody.translatedText;
+
+    // the translation removes whitespace (which included linebreaks). We cannot magically add linebreaks within the sentence,
+    // but we can at least add it at the beginning and at the end of sentences.
+    let leadingWhitespace = sentence.match("^\\s*")
+    let trailingWhitespace = sentence.match("\\s*$")
+    translatedSentence = leadingWhitespace + translatedSentence + trailingWhitespace;
+
     setCachedSentence(sentence, translatedSentence, srcLang, dstLang)
-
     updateOutput();
 }
 
